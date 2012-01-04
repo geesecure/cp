79e6d3fd235abad5e567868966fb08b5491e6572
GopinathSivalingam <geesecure@gmail.com>
2012-01-04 12:48:35 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cp::Application.config.secret_token = 'fcb4c65811854ecd70b0a24bf172cdb0880fe2c6d8cdcb7422c87f787bc60e46b1054645ae0e7d84769c2f8d6634df4d2c4265f258dc3f11eb132c2a03a84369'
